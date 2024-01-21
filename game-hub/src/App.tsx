818f07b3cbe3d670cb1deb147996bff6a725be51
ayanami1314​​ <Ayanami-wyh@outlook.com>
2024-01-21 10:41:25 +0800

    Build a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
+  const templateAreas = {
+    base: `"nav" "main"`, // 移动端小屏幕没有aside侧边栏
+    lg: `"nav nav" "aside main"`, // > 1000px
+  };
   return (
     <div className="App">
-      <Button colorScheme="blue">Button</Button>
+      <Grid templateAreas={templateAreas}>
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 }
