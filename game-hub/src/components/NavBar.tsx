93f56eb4f091f0cdb19cd7b071f996e426d77804
ayanami1314​​ <Ayanami-wyh@outlook.com>
2024-01-21 11:23:56 +0800

    finish the colormode switch in NavBar

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,11 +1,16 @@
 import { HStack } from "@chakra-ui/react";
 import { Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo-black.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize={"80px"}></Image>
-      <Text> GameHub</Text>
+    <HStack justifyContent={"space-between"} padding="20px">
+      <HStack>
+        <Image src={logo} boxSize={"80px"}></Image>
+        <Text>GameHub</Text>
+      </HStack>
+
+      <ColorModeSwitch />
     </HStack>
   );
 };
